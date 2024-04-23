0f12fe8918a9e187c079965e3027528e33793749
Juhtrento <julianelli.d.trento@gmail.com>
2024-04-23 16:37:55 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Julianelli Donato Trento"
+idade=15
+email='julianelli.d.trento@gmail.com'
+telefone='17 99999-0000'
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
