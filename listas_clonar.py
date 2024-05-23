4d73795d5cd01487e7c3c13f001136cea954e90c
Juhtrento <julianelli.d.trento@gmail.com>
2024-05-23 10:22:39 -0300

    listas_comparar.py

---
+++ listas_clonar.py
@@ -0,0 +1,6 @@
+alunos=['João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana']
+copia = list(alunos)
+print(f"Lista Original: {alunos}")
+print(F"Lista Cópia: {copia}")
+del alunos[4]
+print(f"Lista atualizada: {alunos}")
